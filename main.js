653f85e883cd25a4f0e313c390916a7d0da99f6f
Baraa Basim <barabasim77@gmail.com>
2023-09-21 16:23:00 +0800

    modal show button

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
+const showButton = document.getElementById("showDialog");
+const dialog = document.getElementById("book-dialog");
+const output = document.querySelector("output");
 const myLibrary = [];
+
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
 
 function Book(title, author, pages, isRead) {
   this.title = title;
