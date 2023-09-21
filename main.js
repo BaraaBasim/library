f147df98b74e7722d569e434c13d47502bb70438
Baraa Basim <barabasim77@gmail.com>
2023-09-21 16:51:05 +0800

    prevent default

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const showButton = document.getElementById("showDialog");
 const dialog = document.getElementById("book-dialog");
 const output = document.querySelector("output");
+const submitButton = document.getElementById("submit");
+const formBook = document.getElementById("form-book");
 const myLibrary = [];
 
 showButton.addEventListener("click", () => {
   dialog.showModal();
+});
+
+dialog.addEventListener("close", (e) => {
+  output.value =
+    dialog.returnValue === "default"
+      ? "No return value."
+      : `ReturnValue: ${dialog.returnValue}.`; // Have to check for "default" rather than empty string
 });
 
 function Book(title, author, pages, isRead) {
@@ -17,6 +26,14 @@ function Book(title, author, pages, isRead) {
   };
 }
 
+// loop through the library and display the books cards
+function displayBooks() {}
+
 function addBookToLibrary() {}
+
+submitButton.addEventListener("click", (event) => {
+  event.preventDefault(); // We don't want to submit this fake form
+  dialog.close();
+});
 
 let theHobbit = new Book("the hobbit", "me", 23, false);
