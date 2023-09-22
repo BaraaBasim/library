b40ba7740ebf9061fb9b4018d19984e870a3be5a
Baraa Basim <barabasim77@gmail.com>
2023-09-22 17:39:37 +0800

    display books and addbooktolibrary

--- main.js
+++ main.js
@@ -27,13 +27,33 @@ function Book(title, author, pages, isRead) {
 }
 
 // loop through the library and display the books cards
-function displayBooks() {}
+function displayBooks() {
+  for (let book of myLibrary) {
+    console.log(book);
+  }
+}
 
-function addBookToLibrary() {}
+// function removeBook(book) {
+//   myLibrary.filter((item) => {
+//     item.title !== book.title;
+//   });
+// }
+
+function addBookToLibrary() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const isRead = document.getElementById("is-read").value;
+  let book = new Book(title, author, pages, isRead);
+  myLibrary.push(book);
+}
 
 submitButton.addEventListener("click", (event) => {
   event.preventDefault(); // We don't want to submit this fake form
+  addBookToLibrary();
   dialog.close();
+  displayBooks();
 });
 
 let theHobbit = new Book("the hobbit", "me", 23, false);
+myLibrary.push(theHobbit);
