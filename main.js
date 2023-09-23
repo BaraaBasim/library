cff5c2476c5900d10b4b3503295b97d9cda5411c
Baraa Basim <barabasim77@gmail.com>
2023-09-23 16:26:19 +0800

    show modal add card

--- main.js
+++ main.js
@@ -19,8 +19,71 @@ class Book {
   }
 }
 
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  addBook(newBook) {
+    if (!this.isInLibrary(newBook)) {
+      this.books.push(newBook);
+    }
+  }
+
+  removeBook(title) {
+    this.books = this.books.filter((book) => book.title !== title);
+  }
+
+  getBook(title) {
+    return this.books.find((book) => book.title === title);
+  }
+
+  isInLibrary(newBook) {
+    return this.books.some((book) => book.title === newBook.title);
+  }
+}
+
+const library = new Library();
+
+const createBookCard = (book) => {
+  const bookCard = document.createElement("div");
+  const title = document.createElement("p");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const buttonGroup = document.createElement("div");
+  const readBtn = document.createElement("button");
+  const removeBtn = document.createElement("button");
+
+  bookCard.classList.add("book-card");
+  buttonGroup.classList.add("button-group");
+  readBtn.classList.add("btn");
+  removeBtn.classList.add("btn");
+  readBtn.onclick = toggleRead;
+  removeBtn.onclick = removeBook;
+
+  title.textContent = `"${book.title}"`;
+  author.textContent = book.author;
+  pages.textContent = `${book.pages} pages`;
+  removeBtn.textContent = "Remove";
+
+  if (book.isRead) {
+    readBtn.textContent = "Read";
+    readBtn.classList.add("btn-light-green");
+  } else {
+    readBtn.textContent = "Not read";
+    readBtn.classList.add("btn-light-red");
+  }
+
+  bookCard.appendChild(title);
+  bookCard.appendChild(author);
+  bookCard.appendChild(pages);
+  buttonGroup.appendChild(readBtn);
+  buttonGroup.appendChild(removeBtn);
+  bookCard.appendChild(buttonGroup);
+  booksGrid.appendChild(bookCard);
+};
+
 const openAddBookModal = () => {
-  formBook.reset();
   modal.classList.add("active");
   overlay.classList.add("active");
 };
@@ -30,27 +93,13 @@ const closeAddBookModal = () => {
   overlay.classList.remove("active");
 };
 
-// loop through the library and display the books cards
-function displayBooks() {
-  for (let book of myLibrary) {
-    console.log(book);
-  }
-}
-
-function addBookToLibrary() {
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const pages = document.getElementById("pages").value;
-  const isRead = document.getElementById("is-read").value;
-  let book = new Book(title, author, pages, isRead);
-  myLibrary.push(book);
-}
+showModal.addEventListener("click", () => {
+  openAddBookModal();
+});
 
 submitButton.addEventListener("click", (event) => {
   event.preventDefault(); // We don't want to submit this fake form
-  addBookToLibrary();
-  dialog.close();
-  displayBooks();
+  closeAddBookModal();
 });
 
 let theHobbit = new Book("the hobbit", "me", 23, false);
